f1996a4dfe8e0e56ebb6c971205e0cfe73fe9607
atomisadev <atomdevelops@proton.me>
2023-09-06 18:51:25 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+export const thanosSort = (arr: number[]): number[] => {
+  let n = arr.length;
+
+  while (true) {
+    let i: number = 0;
+
+    while (i < n - 1 && arr[i] <= arr[i + 1]) {
+      i++;
+    }
+
+    if (i === n - 1) return arr.slice(0, n);
+
+    n = Math.floor(n / 2);
+    arr = arr.slice(0, n);
+  }
+};
+
+console.log(thanosSort([1, 2, 3, 4, 9, 6]));
